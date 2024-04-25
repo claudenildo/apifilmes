46f9a36fd70678c4e68b2163c39dc44ee644edac
Claudenildo <claudenildo12@hotmail.com>
2024-04-25 19:34:58 +0000

    Novas modificações de fotos

--- movie-app/src/MovieList.js
+++ movie-app/src/MovieList.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 
 const MovieList = () => {
   const [movies, setMovies] = useState([]);
+  const [sortType, setSortType] = useState('title-asc');
 
   useEffect(() => {
     const fetchMovies = async () => {
@@ -13,23 +14,49 @@ const MovieList = () => {
     fetchMovies();
   }, []);
 
+  const sortedMovies = [...movies];
+
+  if (sortType === 'title-asc') {
+    sortedMovies.sort((a, b) => a.title.localeCompare(b.title));
+  } else if (sortType === 'title-desc') {
+    sortedMovies.sort((a, b) => b.title.localeCompare(a.title));
+  } else if (sortType === 'rating-asc') {
+    sortedMovies.sort((a, b) => a.vote_average - b.vote_average);
+  } else if (sortType === 'rating-desc') {
+    sortedMovies.sort((a, b) => b.vote_average - a.vote_average);
+  }
+
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Title</th>
-          <th>IMDB Rating</th>
-        </tr>
-      </thead>
-      <tbody>
-        {movies.map((movie) => (
-          <tr key={movie.id}>
-            <td>{movie.title}</td>
-            <td>{movie.vote_average}</td>
+    <div>
+      <label>
+        Sort by:
+        <select value={sortType} onChange={(e) => setSortType(e.target.value)}>
+          <option value="title-asc">Title (A-Z)</option>
+          <option value="title-desc">Title (Z-A)</option>
+          <option value="rating-asc">Rating (Lowest to Highest)</option>
+          <option value="rating-desc">Rating (Highest to Lowest)</option>
+        </select>
+      </label>
+      <table>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>IMDB Rating</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {sortedMovies.map((movie) => (
+            <tr key={movie.id}>
+              <td>
+                <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} style={{ width: '200px', height: '300px' }} />
+                <a href={`https://www.themoviedb.org/movie/${movie.id}`} target="_blank" rel="noopener noreferrer">{movie.title}</a>
+              </td>
+              <td>{movie.vote_average}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
